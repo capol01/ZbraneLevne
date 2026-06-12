c6caaa25f1fa9467f49176f34ccb101efbbdb1ad
capol01 <capolvoking@gmail.com>
2026-06-12 15:50:37 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 const SUPABASE_URL = "https://github.com/capol01/ZbraneLevne";
-const SUPABASE_ANON_KEY = "sb_publishable_copz8it1bfAkMsnPUH9cPg_X4oBmM2x";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwdm1sZ2pjaWZ6aHhwZWpoZGRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyNTk3MzAsImV4cCI6MjA5NjgzNTczMH0.quFzs2LRabOtx-i2dPJqOTiZYqFk2nmg7vjTM_xNzSU";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
