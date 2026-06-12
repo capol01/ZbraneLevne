fa32bebb4bd33ad987d54d6510edc26b9679e8b9
capol01 <capolvoking@gmail.com>
2026-06-12 21:14:18 +0200

    app.js

---
+++ js/app.js
@@ -0,0 +1,31 @@
+import { supabase } from "./supabase.js";
+
+async function loadProducts() {
+  const container = document.getElementById("product-list");
+
+  const { data, error } = await supabase
+    .from("products")
+    .select("*")
+    .eq("visible", true);
+
+  if (error) {
+    container.innerHTML = "❌ chyba načítání produktů";
+    return;
+  }
+
+  container.innerHTML = "";
+
+  data.forEach((product) => {
+    container.innerHTML += `
+      <div class="product">
+        <img src="${product.image_url}" alt="" style="width:100%; border-radius:10px;">
+        <h3>${product.name}</h3>
+        <p>💰 ${product.price} Kč</p>
+        <p>📦 Skladem: ${product.stock} ks</p>
+        <p>🏷 Kategorie: ${product.category}</p>
+      </div>
+    `;
+  });
+}
+
+loadProducts();
